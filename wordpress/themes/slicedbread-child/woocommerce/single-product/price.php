b0abe8ea75d4ba4ee38b80916a632b29a9cee32e
lew-hardy <lew.hardy@gmail.com>
2026-02-07 11:26:19 +0000

    add popup button add popup remove price

---
+++ wordpress/themes/slicedbread-child/woocommerce/single-product/price.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Single Product Price
+ *
+ * Overridden in child theme to remove price for quote-only PDP.
+ *
+ * @package WooCommerce\Templates
+ * @version 3.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+// Intentionally left blank – price removed for quote-only flow.
